2105f87ac741c54d34361cec7b30fcbb6d1b4d4d
Jacopo <jacopogithub.pbcc5@aleeas.com>
2023-01-21 19:36:14 +0100

    added MB token

--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -1,26 +1,4 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export interface Proposal {
-  'id' : bigint,
-  'title' : string,
-  'winner' : [] | [boolean],
-  'description' : string,
-  'author' : Principal,
-}
-export interface Vote {
-  'id' : bigint,
-  'votingPowah' : number,
-  'voter' : Principal,
-  'vote' : boolean,
-  'idProposal' : bigint,
-}
-export interface _SERVICE {
-  'getVote' : ActorMethod<[bigint], Array<Vote>>,
-  'getVoteFromPrincipal' : ActorMethod<[Principal, bigint], Array<Vote>>,
-  'get_all_proposals' : ActorMethod<[], Array<Proposal>>,
-  'get_all_votes' : ActorMethod<[], Array<Vote>>,
-  'get_proposal' : ActorMethod<[bigint], Array<Proposal>>,
-  'submit_proposal' : ActorMethod<[string, string], bigint>,
-  'vote' : ActorMethod<[bigint, boolean], [] | [bigint]>,
-}
+export interface _SERVICE { 'whoami' : ActorMethod<[], Principal> }
--- src/declarations/backend/backend.did.js
+++ src/declarations/backend/backend.did.js
@@ -1,30 +1,4 @@
 export const idlFactory = ({ IDL }) => {
-  const Vote = IDL.Record({
-    'id' : IDL.Nat,
-    'votingPowah' : IDL.Float64,
-    'voter' : IDL.Principal,
-    'vote' : IDL.Bool,
-    'idProposal' : IDL.Nat,
-  });
-  const Proposal = IDL.Record({
-    'id' : IDL.Nat,
-    'title' : IDL.Text,
-    'winner' : IDL.Opt(IDL.Bool),
-    'description' : IDL.Text,
-    'author' : IDL.Principal,
-  });
-  return IDL.Service({
-    'getVote' : IDL.Func([IDL.Nat], [IDL.Vec(Vote)], ['query']),
-    'getVoteFromPrincipal' : IDL.Func(
-        [IDL.Principal, IDL.Nat],
-        [IDL.Vec(Vote)],
-        ['query'],
-      ),
-    'get_all_proposals' : IDL.Func([], [IDL.Vec(Proposal)], ['query']),
-    'get_all_votes' : IDL.Func([], [IDL.Vec(Vote)], ['query']),
-    'get_proposal' : IDL.Func([IDL.Nat], [IDL.Vec(Proposal)], ['query']),
-    'submit_proposal' : IDL.Func([IDL.Text, IDL.Text], [IDL.Nat], []),
-    'vote' : IDL.Func([IDL.Nat, IDL.Bool], [IDL.Opt(IDL.Nat)], []),
-  });
+  return IDL.Service({ 'whoami' : IDL.Func([], [IDL.Principal], ['query']) });
 };
 export const init = ({ IDL }) => { return []; };
